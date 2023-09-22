4595f1078c793d95d9e5b740626d4ece03b7d646
Evan Sosenko <evan@getseam.com>
2023-09-22 16:42:14 -0700

    Update SeamHttpLegacyWorkspaces

--- src/lib/seam/connect/legacy/workspaces.ts
+++ src/lib/seam/connect/legacy/workspaces.ts
@@ -1,26 +1,40 @@
 import type { RouteRequestParams, RouteResponse } from '@seamapi/types/connect'
+import { Axios } from 'axios'
 import type { SetNonNullable } from 'type-fest'
 
-import { SeamHttpWorkspaces } from 'lib/seam/connect/routes/workspaces.js'
+import { createAxiosClient } from 'lib/seam/connect/axios.js'
+import type { SeamHttpOptions } from 'lib/seam/connect/client-options.js'
+import { parseOptions } from 'lib/seam/connect/parse-options.js'
 
-export class SeamHttpLegacyWorkspaces extends SeamHttpWorkspaces {
-  override async get(
+export class SeamHttpLegacyWorkspaces {
+  client: Axios
+
+  constructor(apiKeyOrOptionsOrClient: Axios | string | SeamHttpOptions) {
+    if (apiKeyOrOptionsOrClient instanceof Axios) {
+      this.client = apiKeyOrOptionsOrClient
+      return
+    }
+
+    const options = parseOptions(apiKeyOrOptionsOrClient)
+    this.client = createAxiosClient(options)
+  }
+
+  async get(
     params: WorkspacesGetParams = {},
   ): Promise<WorkspacesGetResponse['workspace']> {
-    const { data } = await this.client.get<WorkspacesGetResponse>(
-      '/workspaces/get',
-      {
-        params,
-      },
-    )
+    const { data } = await this.client.request<WorkspacesGetResponse>({
+      url: '/workspaces/get',
+      method: 'get',
+      params,
+    })
     return data.workspace
   }
 }
 
-export type WorkspacesGetParams = SetNonNullable<
+type WorkspacesGetParams = SetNonNullable<
   Required<RouteRequestParams<'/workspaces/get'>>
 >
 
-export type WorkspacesGetResponse = SetNonNullable<
+type WorkspacesGetResponse = SetNonNullable<
   Required<RouteResponse<'/workspaces/get'>>
 >
