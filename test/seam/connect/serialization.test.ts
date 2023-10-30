f1eca216608eefc4a8730e9e0db86c08751dd21b
Evan Sosenko <evan@getseam.com>
2023-10-30 15:23:58 -0700

    test: Add explicit get request array serialization test

--- test/seam/connect/serialization.test.ts
+++ test/seam/connect/serialization.test.ts
@@ -1,7 +1,7 @@
 import test from 'ava'
 import { getTestServer } from 'fixtures/seam/connect/api.js'
 
-import { SeamHttp } from '@seamapi/http/connect'
+import { type DevicesListResponse, SeamHttp } from '@seamapi/http/connect'
 
 test('serializes array params when undefined', async (t) => {
   const { seed, endpoint } = await getTestServer(t)
@@ -27,6 +27,44 @@ test('serializes array params when non-empty', async (t) => {
   const devices = await seam.devices.list({
     device_ids: [seed.august_device_1, seed.ecobee_device_1],
   })
+  t.is(devices.length, 2)
+  t.true(devices.some((d) => d.device_id === seed.august_device_1))
+  t.true(devices.some((d) => d.device_id === seed.ecobee_device_1))
+})
+
+test('serializes array params when undefined and explicitly using get', async (t) => {
+  const { seed, endpoint } = await getTestServer(t)
+  const seam = SeamHttp.fromApiKey(seed.seam_apikey1_token, { endpoint })
+  const { data } = await seam.client.get<DevicesListResponse>('/devices/list', {
+    params: {
+      device_ids: undefined,
+    },
+  })
+  const devices = data?.devices
+  t.is(devices.length, 4)
+})
+
+test('serializes array params when empty and explicitly using get', async (t) => {
+  const { seed, endpoint } = await getTestServer(t)
+  const seam = SeamHttp.fromApiKey(seed.seam_apikey1_token, { endpoint })
+  const { data } = await seam.client.get<DevicesListResponse>('/devices/list', {
+    params: {
+      device_ids: [],
+    },
+  })
+  const devices = data?.devices
+  t.is(devices.length, 0)
+})
+
+test('serializes array params when non-empty and explicitly using get', async (t) => {
+  const { seed, endpoint } = await getTestServer(t)
+  const seam = SeamHttp.fromApiKey(seed.seam_apikey1_token, { endpoint })
+  const { data } = await seam.client.get<DevicesListResponse>('/devices/list', {
+    params: {
+      device_ids: [seed.august_device_1, seed.ecobee_device_1],
+    },
+  })
+  const devices = data?.devices
   t.is(devices.length, 2)
   t.true(devices.some((d) => d.device_id === seed.august_device_1))
   t.true(devices.some((d) => d.device_id === seed.ecobee_device_1))
