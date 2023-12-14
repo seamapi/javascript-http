4d73161e9f2ed512c1637a24306a2f48648342e4
Evan Sosenko <evan@getseam.com>
2023-12-14 11:34:46 -0800

    Simplify serialize

--- src/lib/params-serializer.ts
+++ src/lib/params-serializer.ts
@@ -28,30 +28,10 @@ export const paramsSerializer: CustomParamsSerializer = (params) => {
 }
 
 const serialize = (k: string, v: unknown): string => {
-  if (v == null) {
-    throw new UnserializableParamError(k, `is ${v} or contains ${v}`)
-  }
-
-  if (typeof v === 'function') {
-    throw new UnserializableParamError(
-      k,
-      'is a function or contains a function',
-    )
-  }
-
-  if (typeof v === 'object') {
-    throw new UnserializableParamError(k, 'is an object or contains an object')
-  }
-
-  if (typeof v === 'symbol') {
-    throw new UnserializableParamError(k, 'is a symbol')
-  }
-
   if (typeof v === 'string') return v.toString()
   if (typeof v === 'number') return v.toString()
   if (typeof v === 'bigint') return v.toString()
   if (typeof v === 'boolean') return v.toString()
-
   throw new UnserializableParamError(k, `is a ${typeof v}`)
 }
 
