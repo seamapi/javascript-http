7286bb3198d99f3ad20fda5f6653eac933449977
Evan Sosenko <evan@getseam.com>
2024-08-05 15:02:13 -0700

    Test personal-access-token

--- test/seam/connect/personal-access-token.test.ts
+++ test/seam/connect/personal-access-token.test.ts
@@ -7,45 +7,35 @@ import {
   SeamHttpMultiWorkspace,
 } from '@seamapi/http/connect'
 
-// UPSTREAM: Fake does not support personal access token.
-// https://github.com/seamapi/fake-seam-connect/issues/126
-test.failing(
-  'SeamHttp: fromPersonalAccessToken returns instance authorized with personalAccessToken',
-  async (t) => {
-    const { seed, endpoint } = await getTestServer(t)
-    const seam = SeamHttp.fromPersonalAccessToken(
-      'seam_at_TODO',
-      seed.seed_workspace_1,
-      {
-        endpoint,
-      },
-    )
-    const device = await seam.devices.get({
-      device_id: seed.august_device_1,
-    })
-    t.is(device.workspace_id, seed.seed_workspace_1)
-    t.is(device.device_id, seed.august_device_1)
-  },
-)
-
-// UPSTREAM: Fake does not support personal access token.
-// https://github.com/seamapi/fake-seam-connect/issues/126
-test.failing(
-  'SeamHttp: constructor returns instance authorized with personalAccessToken',
-  async (t) => {
-    const { seed, endpoint } = await getTestServer(t)
-    const seam = new SeamHttp({
-      personalAccessToken: 'seam_at_TODO',
-      workspaceId: seed.seed_workspace_1,
+test('SeamHttp: fromPersonalAccessToken returns instance authorized with personalAccessToken', async (t) => {
+  const { seed, endpoint } = await getTestServer(t)
+  const seam = SeamHttp.fromPersonalAccessToken(
+    seed.seam_at1_token,
+    seed.seed_workspace_1,
+    {
       endpoint,
-    })
-    const device = await seam.devices.get({
-      device_id: seed.august_device_1,
-    })
-    t.is(device.workspace_id, seed.seed_workspace_1)
-    t.is(device.device_id, seed.august_device_1)
-  },
-)
+    },
+  )
+  const device = await seam.devices.get({
+    device_id: seed.august_device_1,
+  })
+  t.is(device.workspace_id, seed.seed_workspace_1)
+  t.is(device.device_id, seed.august_device_1)
+})
+
+test('SeamHttp: constructor returns instance authorized with personalAccessToken', async (t) => {
+  const { seed, endpoint } = await getTestServer(t)
+  const seam = new SeamHttp({
+    personalAccessToken: seed.seam_at1_token,
+    workspaceId: seed.seed_workspace_1,
+    endpoint,
+  })
+  const device = await seam.devices.get({
+    device_id: seed.august_device_1,
+  })
+  t.is(device.workspace_id, seed.seed_workspace_1)
+  t.is(device.device_id, seed.august_device_1)
+})
 
 test('SeamHttp: checks personalAccessToken format', (t) => {
   const workspaceId = 'e4203e37-e569-4a5a-bfb7-e3e8de66161d'
@@ -74,22 +64,17 @@ test('SeamHttp: checks personalAccessToken format', (t) => {
   })
 })
 
-// UPSTREAM: Fake does not support personal access token.
-// https://github.com/seamapi/fake-seam-connect/issues/126
-test.failing(
-  'SeamHttpMultiWorkspace: fromPersonalAccessToken returns instance authorized with personalAccessToken',
-  async (t) => {
-    const { endpoint } = await getTestServer(t)
-    const seam = SeamHttpMultiWorkspace.fromPersonalAccessToken(
-      'seam_at_TODO',
-      {
-        endpoint,
-      },
-    )
-    const workspaces = await seam.workspaces.list()
-    t.true(workspaces.length > 0)
-  },
-)
+test('SeamHttpMultiWorkspace: fromPersonalAccessToken returns instance authorized with personalAccessToken', async (t) => {
+  const { seed, endpoint } = await getTestServer(t)
+  const seam = SeamHttpMultiWorkspace.fromPersonalAccessToken(
+    seed.seam_at1_token,
+    {
+      endpoint,
+    },
+  )
+  const workspaces = await seam.workspaces.list()
+  t.true(workspaces.length > 0)
+})
 
 // UPSTREAM: Fake does not support personal access token.
 // https://github.com/seamapi/fake-seam-connect/issues/126
