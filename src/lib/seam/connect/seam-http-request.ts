f01167595c7cc1a7cef019fe29e8423770951c40
Charles Pick <charles@codemix.com>
2025-02-18 23:43:42 +0000

    Immutable SeamPaginator

--- src/lib/seam/connect/seam-http-request.ts
+++ src/lib/seam/connect/seam-http-request.ts
@@ -38,8 +38,6 @@ export class SeamHttpRequest<
   readonly #parent: SeamHttpRequestParent
   readonly #config: SeamHttpRequestConfig<TResponseKey>
 
-  #pagination: Pagination | null = null
-
   constructor(
     parent: SeamHttpRequestParent,
     config: SeamHttpRequestConfig<TResponseKey>,
@@ -79,31 +77,24 @@ export class SeamHttpRequest<
     return this.#config.method
   }
 
-  public get body(): unknown {
-    return this.#config.body
+  public get params(): undefined | Record<string, unknown> {
+    return this.#config.params
   }
 
-  public get pagination(): Pagination | null {
-    return this.#pagination
+  public get body(): unknown {
+    return this.#config.body
   }
 
   async execute(): Promise<
     TResponseKey extends keyof TResponse ? TResponse[TResponseKey] : undefined
   > {
-    const { client } = this.#parent
-    const response = await client.request({
-      url: this.#config.path,
-      method: this.#config.method,
-      data: this.#config.body,
-      params: this.#config.params,
-    })
+    const response = await this.fetchResponseData()
     if (this.responseKey === undefined) {
       return undefined as TResponseKey extends keyof TResponse
         ? TResponse[TResponseKey]
         : undefined
     }
-    if ('pagination' in response.data) this.#pagination = this.pagination
-    const data = response.data[this.responseKey]
+    const data = response[this.responseKey] as any
     if (this.responseKey === 'action_attempt') {
       const waitForActionAttempt =
         this.#config.options?.waitForActionAttempt ??
@@ -111,7 +102,7 @@ export class SeamHttpRequest<
       if (waitForActionAttempt !== false) {
         return await resolveActionAttempt(
           data,
-          SeamHttpActionAttempts.fromClient(client, {
+          SeamHttpActionAttempts.fromClient(this.#parent.client, {
             ...this.#parent.defaults,
             waitForActionAttempt: false,
           }),
@@ -120,6 +111,17 @@ export class SeamHttpRequest<
       }
     }
     return data
+  }
+
+  async fetchResponseData(): Promise<TResponse> {
+    const { client } = this.#parent
+    const response = await client.request({
+      url: this.#config.path,
+      method: this.#config.method,
+      data: this.#config.body,
+      params: this.#config.params,
+    })
+    return response.data
   }
 
   async then<
--- test/seam/connect/seam-pagination.test.ts
+++ test/seam/connect/seam-pagination.test.ts
@@ -3,12 +3,42 @@ import { getTestServer } from 'fixtures/seam/connect/api.js'
 
 import { SeamHttp, SeamPaginator } from '@seamapi/http/connect'
 
-test('SeamHttp: creates a SeamPaginator', async (t) => {
+test('SeamPaginator: creates a SeamPaginator', async (t) => {
   const { seed, endpoint } = await getTestServer(t)
   const seam = SeamHttp.fromApiKey(seed.seam_apikey1_token, { endpoint })
   const pages = seam.createPaginator(seam.devices.list())
   t.true(pages instanceof SeamPaginator)
+})
+
+test('SeamPaginator: fetches an array of devices', async (t) => {
+  const { seed, endpoint } = await getTestServer(t)
+  const seam = SeamHttp.fromApiKey(seed.seam_apikey1_token, { endpoint })
+  const pages = seam.createPaginator(seam.devices.list())
 
   const devices = await pages.toArray()
   t.true(devices.length > 1)
+})
+
+test('SeamPaginator: flattens an array of devices', async (t) => {
+  const { seed, endpoint } = await getTestServer(t)
+  const seam = SeamHttp.fromApiKey(seed.seam_apikey1_token, { endpoint })
+  const pages = seam.createPaginator(seam.devices.list())
+
+  const devices = []
+  for await (const device of pages.flatten()) {
+    devices.push(device)
+  }
+  t.true(devices.length > 1)
+})
+
+test('SeamPaginator: Fetches an array of pages', async (t) => {
+  const { seed, endpoint } = await getTestServer(t)
+  const seam = SeamHttp.fromApiKey(seed.seam_apikey1_token, { endpoint })
+  const pages = seam.createPaginator(seam.devices.list())
+
+  const devices = []
+  for await (const page of pages) {
+    devices.push(page)
+  }
+  t.true(devices.length > 0)
 })
