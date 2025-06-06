03ae6c2fa2997ec9340255c301a84477a6db754b
Evan Sosenko <evan@getseam.com>
2025-06-05 17:05:32 -0700

    Add smith codgen

---
+++ codegen/lib/connect.ts
@@ -0,0 +1,39 @@
+import type { Blueprint, Route } from '@seamapi/blueprint'
+import { kebabCase, pascalCase } from 'change-case'
+import type Metalsmith from 'metalsmith'
+
+interface Metadata {
+  blueprint: Blueprint
+}
+
+type File = RouteLayoutContext & { layout: string }
+
+const rootPath = 'src/lib/seam/connect/routes'
+
+export const connect = (
+  files: Metalsmith.Files,
+  metalsmith: Metalsmith,
+): void => {
+  const metadata = metalsmith.metadata() as Metadata
+  const { blueprint } = metadata
+
+  for (const route of Object.values(blueprint.routes ?? {})) {
+    const k = `${rootPath}/${kebabCase(route.name)}.ts`
+    files[k] = { contents: Buffer.from('\n') }
+    const file = files[k] as unknown as File
+    file.layout = 'route.hbs'
+    setRouteLayoutContext(file, route, blueprint)
+  }
+}
+
+interface RouteLayoutContext {
+  routeName: string
+}
+
+const setRouteLayoutContext = (
+  file: Partial<RouteLayoutContext>,
+  route: Route,
+  _blueprint: Blueprint,
+): void => {
+  file.routeName = pascalCase(route.name)
+}
